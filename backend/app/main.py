c707b60e926e08dc7932aee5108bc05fc0effbbe
arjula075 <ari.iska.lahti@gmail.com>
2026-03-19 15:14:18 +0200

    testing #2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,14 @@
+from __future__ import annotations
+
 from fastapi import FastAPI
 
-app = FastAPI()
+print("MAIN: before api import")
+from .api import router
+print("MAIN: after api import")
+
+app = FastAPI(title="Ristiseiska API")
+
+app.include_router(router)
 
 @app.get("/")
 def root():
